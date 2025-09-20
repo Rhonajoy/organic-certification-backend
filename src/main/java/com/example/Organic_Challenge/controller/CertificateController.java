15a78d71d725bc32897181cd4c607380877e5a87
RhonaJoy <rhonajoy8@gmail.com>
2025-09-21 02:59:21 +0300

    add cert configs

--- src/main/java/com/example/Organic_Challenge/controller/CertificateController.java
+++ src/main/java/com/example/Organic_Challenge/controller/CertificateController.java
@@ -1,18 +1,22 @@
 package com.example.Organic_Challenge.controller;
 
+import com.example.Organic_Challenge.entity.Certificate;
+import com.example.Organic_Challenge.exceptions.ResourceNotFoundException;
+import com.example.Organic_Challenge.repository.CertificateRepository;
 import com.lowagie.text.DocumentException;
 import com.example.Organic_Challenge.dto.CertificateResponseDto;
 import com.example.Organic_Challenge.dto.CreateCertificateDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.example.Organic_Challenge.service.CertificateService;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 
 @RestController
 @RequestMapping("/organic-certified/certificate")
@@ -20,10 +24,31 @@ import java.io.IOException;
 public class CertificateController {
     @Autowired
     CertificateService certificateService;
+    @Autowired
+    CertificateRepository certificateRepository;
 
     @PostMapping
     public ResponseEntity<CertificateResponseDto> generateCertificate(@RequestBody CreateCertificateDto dto) throws DocumentException, IOException {
         CertificateResponseDto certificate = certificateService.generateCertificate(dto);
         return new ResponseEntity<>(certificate, HttpStatus.CREATED);
     }
+    @GetMapping("/download/{certificateId}")
+    public ResponseEntity<byte[]> downloadCertificate(@PathVariable Long certificateId) throws IOException {
+        Certificate cert = certificateRepository.findById(certificateId)
+                .orElseThrow(() -> new ResourceNotFoundException("Certificate not found"));
+
+        File file = new File(cert.getPdfUrl());
+        if (!file.exists()) {
+            throw new ResourceNotFoundException("PDF not found on server.");
+        }
+
+        byte[] contents = Files.readAllBytes(file.toPath());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        headers.setContentDispositionFormData("attachment", file.getName());
+
+        return new ResponseEntity<>(contents, headers, HttpStatus.OK);
+    }
+
 }
