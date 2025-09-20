a67d98bc2e382dbcbc4712c58f6c4a4c156b12d2
RhonaJoy <rhonajoy8@gmail.com>
2025-09-20 22:57:41 +0300

    add cors

---
+++ src/main/java/com/example/Organic_Challenge/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.example.Organic_Challenge.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // applies to all endpoints
+                        .allowedOrigins("http://localhost:5173") // your frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
