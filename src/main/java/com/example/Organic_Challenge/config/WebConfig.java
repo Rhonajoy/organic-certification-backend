562583407527447130c73f335803370d9313334b
RhonaJoy <rhonajoy8@gmail.com>
2025-09-21 21:03:47 +0300

    fix cors

--- src/main/java/com/example/Organic_Challenge/config/WebConfig.java
+++ src/main/java/com/example/Organic_Challenge/config/WebConfig.java
@@ -13,7 +13,8 @@ public class WebConfig {
 
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173",
+                                "https://organic-certification-frontend-kappa.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
